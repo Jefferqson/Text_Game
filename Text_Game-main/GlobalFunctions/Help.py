765a8dff59856cbbcc21671f18a7b4175d877c93
Jefferqson <151244034+Jefferqson@users.noreply.github.com>
2023-12-16 19:35:57 -0500

    Delete Text_Game-main directory

--- Text_Game-main/GlobalFunctions/Help.py
+++
@@ -1,9 +0,0 @@
-def help_function():
-    return ('Global commands:\n'
-            'help -- pull up this menu\n'
-            'inventory -- check inventory\n'
-            'location -- check location\n'
-            'look around -- tells you what locations you can go to\n'
-            '[location] -- go to that location if it is nearby\n'
-            'quit -- will end the game\n'
-            'toss hat -- probably the best way to start. I wonder what will happen?')

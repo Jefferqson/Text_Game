2d06e67e9a9f32cac4f13accc8df7662620e4faa
Jefferqson <ryan.wesdock@gmail.com>
2023-12-16 19:38:25 -0500

    Updated with basic tkinter user interface

--- GlobalFunctions/Help.py
+++
@@ -1,8 +0,0 @@
-def help_function():
-    return ('Global commands:\n'
-            'help - pull up this menu\n'
-            'inventory - check inventory\n'
-            '[location] - go to that location if it is nearby\n'
-            'location - check location\n'
-            'look around - tells you what locations you can go to\n'
-            'toss hat - probably the best way to start. I wonder what will happen?')
